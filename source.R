d0b6bdb535110cb3c6c13aa188624274c0812f50
Sean Swift <seanos@hawaii.edu>
2024-11-03 15:58:18 -1000

    initial commit, base map, zoom map, image overlay, minimal documentation

---
+++ source.R
@@ -0,0 +1,17 @@
+
+
+# quickly plot bounding box using leaflet
+plot_bb = function(bbox){
+# plot the map extent 
+leaflet() %>%
+  addTiles() %>% 
+  addRectangles(
+    lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
+    lng2 = bbox$p2$long, lat2 = bbox$p2$lat,
+    fillColor = "transparent"
+  ) %>%
+  fitBounds(
+    lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
+    lng2 = bbox$p2$long, lat2 = bbox$p2$lat
+  )
+}
